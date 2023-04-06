7804a1ec98d8a7253ec9c4057e48826c4cac94da
franx <miigo@duck.com>
2023-04-06 20:33:55 +0700

    Generic Class, It's Super Cool man

--- src/franxx/code/generic/application/MainGeneric.java
+++ src/franxx/code/generic/application/MainGeneric.java
@@ -1,4 +1,16 @@
 package franxx.code.generic.application;
 
+import franxx.code.generic.MyData;
+
 public class MainGeneric {
+    public static void main(String[] args) {
+        MyData<String> data = new MyData<String>("It's String Man!!");
+        MyData<Integer> integerMyData = new MyData<Integer>(911);
+
+        String s = data.getData();
+        Integer integer = integerMyData.getData();
+
+        System.out.println(s);
+        System.out.println(integer);
+    }
 }
---
+++ src/franxx/code/generic/application/PairApp.java
@@ -0,0 +1,12 @@
+package franxx.code.generic.application;
+
+import franxx.code.generic.Pair;
+
+public class PairApp {
+    public static void main(String[] args) {
+        Pair<String, Integer> pair = new Pair<String, Integer>("Hilmi", 220);
+
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+    }
+}
