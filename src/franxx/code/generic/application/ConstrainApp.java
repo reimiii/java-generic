948d5a3c6b06453846074deb0460c55e914e81ba
franx <miigo@duck.com>
2023-04-07 16:41:03 +0700

    Constrain

---
+++ src/franxx/code/generic/application/ConstrainApp.java
@@ -0,0 +1,26 @@
+package franxx.code.generic.application;
+
+public class ConstrainApp {
+    public static void main(String[] args) {
+        NumberData<Integer> data = new NumberData<>(122);
+        NumberData<Long> longNumberData = new NumberData<>(1929L);
+
+        // NumberData<String> stringNumberData = new NumberData<String>("Hell"); // ERROR
+    }
+
+    public static class NumberData<T extends Number> {
+        private T data;
+
+        public NumberData(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
+}
