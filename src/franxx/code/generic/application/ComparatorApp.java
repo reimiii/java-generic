0d4f8438e5dcf57d2206e9a4df50dd9b1fdd8b61
franxx <miigo@duck.com>
2023-04-08 14:45:59 +0700

    Done!!

---
+++ src/franxx/code/generic/application/ComparatorApp.java
@@ -0,0 +1,27 @@
+package franxx.code.generic.application;
+
+import franxx.code.generic.Person;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ComparatorApp {
+    public static void main(String[] args) {
+        Person[] people = {
+                new Person("Hilmi", "Bogor"),
+                new Person("Arip", "Bogor"),
+                new Person("Torik", "Bogor")
+        };
+
+        Comparator<Person> comparator = new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+
+        Arrays.sort(people, comparator);
+
+        System.out.println(Arrays.toString(people));
+    }
+}
